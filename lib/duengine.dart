cb03cbd701249a86bcb3871bde0828829844a6e2
cantidio <aniquilatorbloody@gmail.com>
2014-09-02 20:22:38 -0300

    Initial commit

---
+++ lib/duengine.dart
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2013 Cantidio Fontes
+ * For conditions of distribution and use, see copyright notice in LICENSE.txt
+ *
+ * This library contains a set of classes that let you create a 2d game.
+ */
+library DuEngine;
+import 'package:gorgon/gorgon.dart';
+
+
+//import 'dart:core';
+//import 'dart:html';
+//import 'dart:async';
+//import 'dart:convert';
+//import 'dart:math';
+//import 'dart:collection';
+//import 'dart:web_audio';
